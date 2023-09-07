b449902aad623844bc1f1f53a53bb6d40e067e92
a-appelblom <a.appelblom@gmail.com>
2023-09-07 15:24:24 +0200

    updates to add quote

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import datetime
+import requests
+from fastapi import FastAPI
 
-from deta import app
 from dotenv import load_dotenv
 from slack_sdk import WebClient
 
@@ -10,16 +11,34 @@ load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL = os.getenv("CHANNEL_ID")
 
-client = WebClient(token=TOKEN)
+app = FastAPI()
 
-date = str(datetime.date.today())
-date = date.split("-")
+@app.post('/__space/v0/actions')
+def actions(request: dict):
+    client = WebClient(token=TOKEN)
 
-message = f"{date[2]}/{date[1]} - {date[0]}"
+    date = str(datetime.date.today())
+    date = date.split("-")
+    api_url = 'https://api.quotable.io/quotes/random'
+    data = requests.get(api_url)
+    data = data.json()[0]
+    myString = f"\n\n {data['content']} \n\n - {data['author']}"
 
-client.chat_postMessage(channel=CHANNEL, text=message)
+    message = f"{date[2]}/{date[1]} - {date[0]} \n\n"
+    message += myString
+    data = request
+    event = data['event']
+    if event['id'] == 'send-message':
+        client.chat_postMessage(channel=CHANNEL, text=message)
 
 
-@app.lib.crono()
-def app(event):
-    client.chat_postMessage(channel=CHANNEL, text=message)
+
+
+
+if __name__ == '__main__':
+    actions({
+        "event": {
+            "id": "send-message",
+            "trigger": "schedule"
+        }
+    })
