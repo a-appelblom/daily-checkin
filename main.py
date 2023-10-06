ee180b5e9e24bc4d4fecabb5dbff84daa167a3de
a-appelblom <a.appelblom@gmail.com>
2023-10-06 19:14:39 +0200

    adds question of the day

--- main.py
+++ main.py
@@ -19,13 +19,20 @@ def actions(request: dict):
 
     date = str(datetime.date.today())
     date = date.split("-")
-    api_url = 'https://api.quotable.io/quotes/random'
-    data = requests.get(api_url)
-    data = data.json()[0]
-    myString = f"\n\n {data['content']} \n\n - {data['author']}"
+    quote_api_url = 'https://api.quotable.io/quotes/random'
+    quote_data = requests.get(quote_api_url)
+    quote_data = quote_data.json()[0]
+    my_quote_string = f"{quote_data['content']} \n\n - {quote_data['author']} \n\n"
+
+    question_api_url = "https://poopoo-api.vercel.app/api/qotd"
+    question_data = requests.get(question_api_url)
+    question_data = question_data.json()
+    question = question_data['question']
+    question_string = f"Question of the day: \n {question}"
 
     message = f"{date[2]}/{date[1]} - {date[0]} \n\n"
-    message += myString
+    message += my_quote_string
+    message += question_string
     data = request
     event = data['event']
     if event['id'] == 'send-message':
